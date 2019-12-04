e92a5a02cea372ee76ab9b765bf6e8468f6fa500
Amaru Cuba Gyllensten <amarucuba@gmail.com>
2019-12-04 21:28:46 +0100

    cleaned up day 4 slightly

--- src/Day4.hs
+++ src/Day4.hs
@@ -9,7 +9,6 @@ import Data.Int
 
 type Password = [Int8]
 
-
 normalizePassword :: Password -> Password
 normalizePassword (x:xs) = x : go x xs
   where
@@ -20,14 +19,11 @@ normalizePassword (x:xs) = x : go x xs
 
 next :: Password -> Password
 next [x] = [x + 1]
-next ps@(x:9:t) = let x' = x+1 in replicate (length ps) x'
+next ps@(x:9:_) = let x' = x+1 in replicate (length ps) x'
 next (x:t) = x : next t
 
-parseDecimal :: Parser Int8
-parseDecimal = read . pure <$> digit
-
 parsePassword :: Parser Password
-parsePassword = count 6 parseDecimal
+parsePassword = count 6 (read . pure <$> digit)
 
 parseProblem :: Parser (Password, Password)
 parseProblem = do
@@ -43,12 +39,7 @@ valid2 :: Password -> Bool
 valid2 = any (==2) . map length . group
 
 countPossible :: (Password -> Bool) -> Password -> Password -> Int
-countPossible valid ub = go 0
-  where 
-    go count x = case (x <= ub, valid x) of
-      (True, True) -> go (count + 1) (next x)
-      (True, False) -> go count (next x)
-      _             -> count
+countPossible valid ub = length . filter valid . takeWhile (<= ub) . iterate next
 
 solve :: String -> IO ()
 solve filepath = do
