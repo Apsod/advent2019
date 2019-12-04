f1defe205dd45b32a1066d04fad57240b9577134
Amaru Cuba Gyllensten <amarucuba@gmail.com>
2019-12-04 21:32:49 +0100

    cleaned up day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -2,20 +2,18 @@ module Day1 (solve) where
 
 import Text.Trifecta
 
-newtype Module = Module {mass :: Integer}
-  deriving(Show)
 
-parseModule :: Parser Module
-parseModule = (Module <$> integer)
+parseModule :: Parser Int
+parseModule = (fromIntegral <$> integer)
 
-mass2fuel :: Integer -> Integer
+mass2fuel :: Int -> Int
 mass2fuel mass = mass `div` 3 - 2
 
-mass2fuel2 :: Integer -> Integer
+mass2fuel2 :: Int -> Int
 mass2fuel2 = sum . takeWhile (> 0) . tail . iterate mass2fuel
 
 solve :: String -> IO ()
 solve filepath = do
   Just xs <- parseFromFile (manyTill parseModule eof) filepath
-  print (sum $ map (mass2fuel . mass) xs)
-  print (sum $ map (mass2fuel2 . mass) xs)
+  print . sum $ map mass2fuel xs
+  print . sum $ map mass2fuel2 xs
